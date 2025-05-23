ea77a69eeb6f8a2bb7b22cb8ea3ddbcf8d9669b1
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-23 09:42:46 +0800

    fix: correct module export syntax in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
-module.exports = {
+
+const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
-const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -19,12 +18,12 @@ const nextConfig: NextConfig = {
         protocol: "https",
         port: "",
       },
-       {
+      {
         hostname: "i.scdn.co",
         protocol: "https",
         port: "",
-      }
-    ]
+      },
+    ],
   },
 };
 
