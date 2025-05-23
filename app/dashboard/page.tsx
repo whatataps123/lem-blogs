04f8cbb7ffb5b9c9d8e207210f88a23dcd539163
Joshua Lemuel Z. Centina <joshualemuelcentina@gmail.com>
2025-05-23 09:28:57 +0800

    fix: handle potential undefined user ID when fetching data in DashboardRoute

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -23,7 +23,7 @@ export default async function DashboardRoute() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  const data = await getData(user!.id);
+  const data = await getData(user?.id as string);
 
   return (
     <div>
